85f182b1eab3e3a1f4ec961609f31e136e450841
CatherineYellow <huangjj2021@mail.sustech.edu.cn>
2025-04-03 14:31:07 +0800

    eval ToRL

--- src/scripts/infer.sh
+++ src/scripts/infer.sh
@@ -1,7 +1,8 @@
 set -ex
-
+MODEL_NAME_OR_PATH="/data/vayu/train/models/ckpts/ToRL/rl.grpo_qwen.base_7b_torl_data_numcall1/global_step_160/actor_hf"
+# MODEL_NAME_OR_PATH="RUC-AIBOX/STILL-3-TOOL-32B"
 # MODEL_NAME_OR_PATH="/data/vayu/train/models/xDAN-L1-Qwen25-7B-Instruct"
-MODEL_NAME_OR_PATH="RUC-AIBOX/STILL-3-1.5B-preview"
+# MODEL_NAME_OR_PATH="RUC-AIBOX/STILL-3-1.5B-preview"
 # MODEL_NAME_OR_PATH="llm-agents/tora-70b-v1.0"
 
 # DATA_LIST = ['math', 'gsm8k', 'gsm-hard', 'svamp', 'tabmwp', 'asdiv', 'mawps']
@@ -11,7 +12,7 @@ DATA_NAME="math"
 
 SPLIT="test"
 PROMPT_TYPE="tora"
-NUM_TEST_SAMPLE=-1
+NUM_TEST_SAMPLE=5
 
 
 CUDA_VISIBLE_DEVICES=0 TOKENIZERS_PARALLELISM=false \
